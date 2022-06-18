9d4fdababf2fc06f2130d6086e4da19190532c7b
Santiago Luna! <kobyj103@gmail.com>
2022-06-17 21:41:10 -0500

    Agregado datos

--- src/com/entregable/DatosJava.java
+++ src/com/entregable/DatosJava.java
@@ -1,4 +1,34 @@
 package com.entregable;
 
 public class DatosJava {
+    public static void main (String[] args) {
+        byte number1 = 1;
+        short number2 = 12;
+        int number3 = 1234;
+        long number4 = 12345678;
+        float number5 = 4.9f;
+        double number6 = 4.99d;
+        boolean stateTrue = true;
+        boolean stateFalse = false;
+        String name = "Santiago Luna";
+
+        System.out.println("Tipo de datos en java");
+
+        System.out.println("numericos.");
+
+        System.out.println("number1= " + number1);
+        System.out.println("number2= " + number2);
+        System.out.println("number3= " + number3);
+        System.out.println("number4= " + number4);
+        System.out.println("number5= " + number5);
+        System.out.println("number6= " + number6);
+
+        System.out.println("Datos booleanos.");
+        System.out.println("Verdadero= " + stateTrue);
+        System.out.println("Falso= " + stateFalse);
+
+        System.out.println("Cadenas.");
+        System.out.println("nombre= " + name);
+
+    }
 }
